dc1cc20b200581e5403a252fca46a70d50fb1acc
Neil Hanak <neil.hanak@gmail.com>
2021-04-08 16:39:55 -0600

    Added next-page-transitions

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import { ThemeContext, themes} from "../components/Theme/Theme";
 import { createGlobalStyle } from 'styled-components';
+import { PageTransition } from 'next-page-transitions'
 
 import PageContainer from "../components/PageContainer/PageContainer";
 
@@ -32,7 +33,7 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
     const [isDarkMode, setDarkMode] = useState(false);
     const [mobileNavbarIsOpen, setMobileNavbarOpen] = useState(false);
 
@@ -44,7 +45,38 @@ function MyApp({ Component, pageProps }) {
     <ThemeContext.Provider value={isDarkMode ? themes.dark : themes.light}>
         <GlobalStyle isDarkMode={isDarkMode} themes={themes} mobileNavbarIsOpen={mobileNavbarIsOpen}/>
         <PageContainer toggleTheme={toggleTheme} isDarkMode={isDarkMode} mobileNavbarIsOpen={mobileNavbarIsOpen} setMobileNavbarOpen={setMobileNavbarOpen}>
-            <Component {...pageProps} />
+        <PageTransition timeout={300} classNames="page-transition">
+            <Component {...pageProps} key={router.route} />
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            animation: moveUp 300ms ease;
+            transform:translateY(0%);
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+
+          @keyframes moveUp {
+            0% {
+                opacity:0;
+                transform:translateY(30%)
+            }
+            100% {
+                opacity:1;
+                transform:translateY(0%)
+            }
+        }
+        `}</style>
         </PageContainer>
     </ThemeContext.Provider>
   )
