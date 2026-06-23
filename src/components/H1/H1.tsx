2ae1b542ddd81d53db2b9d220bbed782b6cb466d
Neil Hanak <neil.hanak@gmail.com>
2026-06-23 10:21:06 -0600

    chore: move P component to be in a more tailwind style

--- src/components/H1/H1.tsx
+++ src/components/H1/H1.tsx
@@ -1,6 +1,6 @@
 export default function H1(props: React.ComponentPropsWithoutRef<"h1">) {
   return (
-    <h1 {...props} className="text-text-primary text-6xl sm:text-7xl">
+    <h1 {...props} className="text-text-primary text-6xl sm:text-7xl font-bold">
       {props.children}
     </h1>
   );
