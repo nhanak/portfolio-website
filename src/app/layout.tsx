4f2a134a65de366b22b4e12f8153260c6f304aa1
Neil Hanak <neil.hanak@gmail.com>
2026-06-23 09:51:13 -0600

    bug: fix more Next.js complaints about theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import Script from "next/script";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <Script id="custom-inline-script" strategy="beforeInteractive">
-        {`
+      <head>
+        <script>
+          {`
             if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                 document.documentElement.classList.add('dark');
             } else {
                 document.documentElement.classList.remove('dark');
             }
           `}
-      </Script>
+        </script>
+      </head>
       <body>{children}</body>
     </html>
   );
