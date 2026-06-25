74dc96f396ea2ef007086d7a14a7b3db0dcc7e77
Neil Hanak <neil.hanak@gmail.com>
2026-06-25 08:01:00 -0600

    refactor: add types to ProjectCard component

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,38 +1,27 @@
-import { PropsWithChildren } from "react";
 import Link from "next/link";
 
-export default function ProjectCard(props) {
+export default function ProjectCard(props: {
+  href: string;
+  src: string;
+  title: string;
+  tags: string;
+}) {
   const { href } = props;
 
   return (
     <div>
       <div className="relative">
         <Link href={href}>
-          <NewProjectCardOverlay />
+          <div className="w-full h-full absolute rounded-lg hover:opacity-20 bg-white opacity-0 ease duration-500" />
           <img src={props.src} className="rounded-lg" />
         </Link>
       </div>
       <Link href={href}>
-        <NewProjectCardTitleTextStyled>
-          {props.title}
-        </NewProjectCardTitleTextStyled>
+        <div className="text-primary pt-2">{props.title}</div>
       </Link>
       <Link href={href}>
-        <NewProjectCardTagTextStyled>{props.tags}</NewProjectCardTagTextStyled>
+        <div className="text-secondary">{props.tags}</div>
       </Link>
     </div>
   );
 }
-const NewProjectCardOverlay = () => {
-  return (
-    <div className="w-full h-full absolute rounded-lg hover:opacity-20 bg-white opacity-0 ease duration-500" />
-  );
-};
-
-const NewProjectCardTitleTextStyled = (props: PropsWithChildren) => {
-  return <div className="text-primary pt-2">{props.children}</div>;
-};
-
-const NewProjectCardTagTextStyled = (props: PropsWithChildren) => {
-  return <div className="text-secondary">{props.children}</div>;
-};
