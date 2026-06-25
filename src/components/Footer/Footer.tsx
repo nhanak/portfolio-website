de82d03c30bf428de531216d5d7fc2d7eaca9ed2
Neil Hanak <neil.hanak@gmail.com>
2026-06-25 08:58:26 -0600

    fix: resolve issue where footer right items underline would be too large

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import PageSectionContainer from "../pageSectionContainer/PageSectionContainer";
 import Link_ from "../linkWithUnderlineAnimation/LinkWithUnderlineAnimation";
-import styled from "styled-components";
+import { PropsWithChildren } from "react";
 
 export default function Footer() {
   return (
     <PageSectionContainer marginTop="50px" borderTop="1px solid grey">
-      <FooterStyled>
-        <FooterStartStyled>
-          <FooterText className="text-primary text-base">
+      <div className="flex justify-between mt-[10px] border-t border-secondary pt-[50px] mb-[25px]">
+        <FooterColumn>
+          <div>
             Made with&nbsp;
             <Link_
               href="https://nextjs.org/"
@@ -24,68 +24,35 @@ export default function Footer() {
             >
               Vercel
             </Link_>
-          </FooterText>
-          <FooterText className="text-primary text-base">
+          </div>
+          <div>
             MIT License © 2021–present{" "}
             <Link_ href="/" initialAccent={true}>
               Neil Hanak
             </Link_>
-          </FooterText>
-          <FooterText className="text-primary text-base">
-            Made with ❤️ in Canada
-          </FooterText>
-        </FooterStartStyled>
-        <FooterEndStyled>
-          <FooterLinkItem>
-            <Link_ href="/projects">Projects</Link_>
-          </FooterLinkItem>
-          <FooterLinkItem>
+          </div>
+          <div>Made with ❤️ in Canada</div>
+        </FooterColumn>
+        <FooterColumn className="text-right">
+          <Link_ href="/projects">Projects</Link_>
+          <div>
             <Link_ href="/blog">Blog</Link_>
-          </FooterLinkItem>
-          <FooterLinkItem>
+          </div>
+          <div>
             <Link_ href="/about">About</Link_>
-          </FooterLinkItem>
-        </FooterEndStyled>
-      </FooterStyled>
+          </div>
+        </FooterColumn>
+      </div>
     </PageSectionContainer>
   );
 }
 
-const FooterStyled = styled.div`
-  display: flex;
-  margin-top: 50px;
-  font-size: 1.6rem;
-  padding-bottom: 50px;
-`;
-
-const FooterStartStyled = styled.div`
-  width: 100%;
-  display: flex;
-  text-align: center;
-  flex-direction: column;
-  align-items: center;
-  @media (min-width: 992px) {
-    width: 50%;
-    align-items: flex-start;
-  }
-`;
-
-const FooterEndStyled = styled.div`
-  width: 0%;
-  display: none;
-  flex-direction: column;
-  align-items: flex-end;
-  @media (min-width: 992px) {
-    display: flex;
-    width: 50%;
-  }
-`;
-
-const FooterText = styled.p`
-  margin: 0px;
-  margin-bottom: 20px;
-`;
-
-const FooterLinkItem = styled.div`
-  margin-bottom: 20px;
-`;
+const FooterColumn = (props: PropsWithChildren & { className?: string }) => {
+  return (
+    <div
+      className={`flex flex-col gap-y-[20px] text-primary text-base ${props.className ?? ""}`}
+    >
+      {props.children}
+    </div>
+  );
+};
