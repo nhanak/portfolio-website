d4e36ffb003cb2816f30f832df48d1b33c79599b
Neil Hanak <neil.hanak@gmail.com>
2026-07-02 09:11:03 -0600

    style: improve footer style on mobile size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,34 @@ import { PropsWithChildren } from "react";
 export default function Footer() {
   return (
     <PageSectionContainer marginTop="50px" borderTop="1px solid grey">
-      <div className="flex justify-between mt-[10px] border-t border-secondary pt-[50px] mb-[25px]">
+      <div className="flex lg:hidden flex-col items-center gap-y-4 text-primary text-base border-t border-secondary pt-8">
+        <div>
+          Made with&nbsp;
+          <Link_
+            href="https://nextjs.org/"
+            initialAccent={true}
+            target="_blank"
+          >
+            Next.js
+          </Link_>
+          , hosted on&nbsp;
+          <Link_
+            href="https://vercel.com/"
+            target="_blank"
+            initialAccent={true}
+          >
+            Vercel
+          </Link_>
+        </div>
+        <div>
+          MIT License © 2021–present{" "}
+          <Link_ href="/" initialAccent={true}>
+            Neil Hanak
+          </Link_>
+        </div>
+        <div>Made with ❤️ in Canada</div>
+      </div>
+      <div className="justify-between mt-[10px] border-t border-secondary pt-[50px] mb-[25px] hidden lg:flex">
         <FooterColumn>
           <div>
             Made with&nbsp;
--- src/components/PageSectionContainer/PageSectionContainer.tsx
+++ src/components/PageSectionContainer/PageSectionContainer.tsx
@@ -8,7 +8,7 @@ export default function PageSectionContainer(
 ) {
   return (
     <div
-      className={`pb-2 sm:pb-8 mb-12 w-full ${props.borderBottomMobile ? "border-secondary lg:border-none border-solid border-b " : ""} ${props.className ?? ""}`}
+      className={`pb-2 sm:pb-2 mb-12 w-full ${props.borderBottomMobile ? "border-secondary lg:border-none border-solid border-b " : ""} ${props.className ?? ""}`}
     >
       {props.children}
     </div>
